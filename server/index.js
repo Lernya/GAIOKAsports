0ce4c105d59f421d9484eebeaa56deb7da2974bd
GitLernya <174644721+Lernya@users.noreply.github.com>
2024-10-01 15:33:02 +0200

    app.get mit Hola Mundo

--- server/index.js
+++ server/index.js
@@ -3,4 +3,5 @@ import express from 'express';
 const app = express();
 const PORT = 8000;
 
+app.get('/', (req, res) => res.send('Hello, World! ¡Hola Mundo!'));
 app.listen(PORT, () => console.log(`Server is running on PORT:${PORT}`));
