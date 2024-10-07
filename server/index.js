bd92983076d7279dfef05b2d9c580367ff807f57
GitLernya <174644721+Lernya@users.noreply.github.com>
2024-10-07 05:57:46 +0200

    API users, users/:id, orders, orders/:id (Basic)

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import './db/index.js';
+import usersRouter from './routers/usersRouter.js';
+import ordersRouter from './routers/ordersRouter.js';
 // wichtig: immer Dateiendung js angeben
 
 const app = express();
@@ -9,7 +11,11 @@ const PORT = 8000;
 // Cors, fuer Parser für json-Body
 
 // Routes
-app.get('/', (req, res) => res.send('Hello, World! ¡Hola Mundo! Hallo Welt!'));
+// app.get('/', (req, res) => res.send('Hello, World! ¡Hola Mundo! Hallo Welt!'));
+// Hier in index.js wird die Hauptroute definiert. Also das, was nach der Domain kommt und VOR dem, was in routers definiert wird.
+// Daher alle Routen in routers haben /users/ als Basis!
+app.use('/users', usersRouter);
+app.use('/orders', ordersRouter);
 
 
 
